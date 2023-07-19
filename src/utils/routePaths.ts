50904bf2046ca53b1d49d151fd637f3eeddff5d5
Oghenetega Esedere <arcteggzz@gmail.com>
2023-07-20 00:58:08 +0100

    fix: corrected missing route in routePaths file

--- src/utils/routePaths.ts
+++ src/utils/routePaths.ts
@@ -2,6 +2,8 @@ const routePaths = {
   SPLASHPAGE: "/",
   HOMEPAGE: "/home",
   ABOUT_US: "/about",
+  GET_A_TICKET: "/get-a-tickect",
+  EVENT_LISTINGS: "/event-listings",
   LOGIN: "/auth/login",
   SIGNUP: "/auth/signup",
   FORGOT_PASSWORD: "/auth/reset-password",
